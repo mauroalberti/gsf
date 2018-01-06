83c8e2ab77d8617d100caba1aac42ad8472d74be
mauroalberti <alberti.m65@gmail.com>
2018-01-06 18:25:55 +0100

    ptbaxes refined

--- gsf/ptbaxes.py
+++ gsf/ptbaxes.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from .arrays import arrays_are_close
 from .geometry import *
 from .faults import Slickenline, FaultSlick
 
@@ -21,7 +22,7 @@ class PTBAxes(object):
         P and T axes (provided as GAxis instances).
 
         Example:
-          >>> PTBAxes(GAxis(0, 0), GAxis(90, 0))
+          >>> PTBAxes(p_axis=GAxis(0, 0), t_axis=GAxis(90, 0))
           PTBAxes(P: GAxis(000.00, +00.00), T: GAxis(090.00, +00.00))
         """
 
@@ -55,16 +56,60 @@ class PTBAxes(object):
         return obj
 
     @property
+    def p_versor(self):
+        """
+        Return the P versor component of the PTBAxes instance.
+
+        :return: P versor instance
+
+        Example:
+          >>> PTBAxes(p_axis=GAxis(0, 0), t_axis=GAxis(90, 0)).p_versor
+          Vect(0.0000, 1.0000, -0.0000)
+        """
+
+        return self._p_versor
+
+    @property
+    def t_versor(self):
+        """
+        Return the T versor component of the PTBAxes instance.
+
+        :return: T versor instance
+
+        Example:
+          >>> PTBAxes(p_axis=GAxis(0, 0), t_axis=GAxis(90, 0)).t_versor
+          Vect(1.0000, 0.0000, -0.0000)
+        """
+
+        return self._t_versor
+
+    @property
+    def b_versor(self):
+        """
+        Return the B versor component of the PTBAxes instance.
+
+        :return: B versor instance
+
+        Example:
+          >>> PTBAxes(p_axis=GAxis(0, 0), t_axis=GAxis(90, 0)).b_versor
+          Vect(0.0000, 0.0000, 1.0000)
+        """
+
+        return self.t_versor.vp(self.p_versor)
+
+    @property
     def p_axis(self):
         """
         Return the P axis.
 
         Example:
-          >>> PTBAxes(GAxis(0, 0), GAxis(90, 0)).p_axis
+          >>> PTBAxes(p_axis=GAxis(0, 0), t_axis=GAxis(90, 0)).p_axis
           GAxis(000.00, +00.00)
+          >>> PTBAxes(p_axis=GAxis(0, 90), t_axis=GAxis(90, 0)).p_axis
+          GAxis(000.00, +90.00)
         """
 
-        return self._p_versor.gaxis()
+        return self.p_versor.gaxis()
 
     @property
     def t_axis(self):
@@ -72,11 +117,13 @@ class PTBAxes(object):
         Return the T axis.
 
         Example:
-          >>> PTBAxes(GAxis(0, 0), GAxis(90, 0)).t_axis
+          >>> PTBAxes(p_axis=GAxis(0, 0), t_axis=GAxis(90, 0)).t_axis
+          GAxis(090.00, +00.00)
+          >>> PTBAxes(p_axis=GAxis(0, -90), t_axis=GAxis(90, 0)).t_axis
           GAxis(090.00, +00.00)
         """
 
-        return self._t_versor.gaxis()
+        return self.t_versor.gaxis()
 
     @property
     def b_axis(self):
@@ -84,11 +131,13 @@ class PTBAxes(object):
         Calculate the B axis.
 
         Example:
-          >>> PTBAxes(GAxis(0, 0), GAxis(90, 0)).b_axis
-          GAxis(000.00, +90.00)
+          >>> PTBAxes(p_axis=GAxis(0, 0), t_axis=GAxis(90, 0)).b_axis
+          GAxis(000.00, -90.00)
+          >>> PTBAxes(p_axis=GAxis(0, 90), t_axis=GAxis(0, 0)).b_axis
+          GAxis(270.00, +00.00)
         """
 
-        return self._p_versor.vp(self._t_versor).gaxis()
+        return self.b_versor.gaxis()
 
     @property
     def m_plane(self):
@@ -96,13 +145,37 @@ class PTBAxes(object):
         Calculate M plane.
 
         Example:
-          >>> PTBAxes(GAxis(0, 90), GAxis(90, 0)).m_plane.almost_parallel(GPlane(0.0, 90.0))
+          >>> PTBAxes(p_axis=GAxis(0, 90), t_axis=GAxis(90, 0)).m_plane.almost_parallel(GPlane(0.0, 90.0))
           True
-          >>> (PTBAxes(GAxis(45, 45), GAxis(225, 45)).m_plane).almost_parallel(GPlane(315.00, 90.00))
+          >>> (PTBAxes(p_axis=GAxis(45, 45), t_axis=GAxis(225, 45)).m_plane).almost_parallel(GPlane(315.00, 90.00))
           True
         """
 
         return self.p_axis.common_plane(self.t_axis)
+
+    def to_matrix(self):
+        """
+        Creates a rotation matrix from the PTB vector components.
+        Formula as in:
+        - eq. 3 in Kagan, Y. Y., 2007. Simplified algorithms for calculating double-couple rotation.
+        - eq. 31 in Kagan, Y. Y., 2008. On geometric complexity of earthquake focal zone and fault system.
+
+        :return: a 3x3 numpy arrays fo floats.
+
+        Example:
+          >>> arrays_are_close(PTBAxes(p_axis=GAxis(0, 0), t_axis=GAxis(90, 0)).to_matrix(), np.identity(3))
+          True
+        """
+
+        t = self.t_versor
+        p = self.p_versor
+        b = self.b_versor
+
+        return np.array([
+            [t.x, p.x, b.x],
+            [t.y, p.y, b.y],
+            [t.z, p.z, b.z]
+        ])
 
 
 if __name__ == "__main__":
