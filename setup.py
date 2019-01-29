5de018748e022aad0c251637c60bedc0df11ec31
mauroalberti <alberti.m65@gmail.com>
2019-01-29 18:36:09 +0100

    modified setup files

--- setup.py
+++ setup.py
@@ -1,3 +1,25 @@
-from setuptools import setup
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-setup(name='pygsf')
+from setuptools import setup, find_packages
+
+setup(
+    name='pygsf',
+    version='3.0.0',
+    packages=find_packages(),
+
+    install_requires=['numpy', 'matplotlib'],
+
+    # metadata for upload to PyPI
+    author="Mauro Alberti",
+    author_email="alberti.m65@gmail.com",
+    description="gsf, a library for structural geology",
+    license="GPL-3",
+    keywords="structural geology",
+    url="https://github.com/mauroalberti/gsf",
+    project_urls={
+        "Bug Tracker": "https://github.com/mauroalberti/gsf/issues",
+        "Documentation": "https://github.com/mauroalberti/gsf/blob/master/README.md",
+        "Source Code": "https://github.com/mauroalberti/gsf/tree/master/gsf_py",
+    }
+)
